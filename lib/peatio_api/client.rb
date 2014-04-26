047d334e2f8b335d82ad8550d50a459e1057969d
Jan Xie <jan.h.xie@gmail.com>
2014-04-26 10:22:56 +0800

    get public api

--- lib/peatio_api/client.rb
+++ lib/peatio_api/client.rb
@@ -1,4 +1,7 @@
+require 'active_support/core_ext'
 require 'active_support/hash_with_indifferent_access'
+require 'json'
+require 'net/http'
 require_relative 'client/version'
 
 module PeatioAPI
@@ -7,6 +10,22 @@ module PeatioAPI
     def initialize(options={})
       options.symbolize_keys!
       setup_auth_keys options
+      @endpoint = options[:endpoint] || 'https://peatio.com'
+    end
+
+    def get(path, params={})
+      uri = URI("#{@endpoint}#{path}")
+      uri.query = URI.encode_www_form(params) if params.present?
+      response = Net::HTTP.get_response(uri)
+
+      if response.code == '200'
+        JSON.parse response.body
+      else
+        raise "Request failed: #{response.body}"
+      end
+    end
+
+    def post(path, params={})
     end
 
     private
